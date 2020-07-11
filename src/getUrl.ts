091ea9eb92e55c60eeb1b6230a4c73bcd5c8f7e8
mail@nixiaobai.com <mail@nixiaobai.com>
2020-07-11 21:01:47 +0800

    添加抖音、B站直播链接获取

--- src/getUrl.ts
+++ src/getUrl.ts
@@ -1,14 +1,17 @@
 //const huya = require('./website/huya');
 //const douyu = require('./website/douyu');
-const kugou = require('./website/kugou');
+//const kugou = require('./website/kugou');
+//const douyin = require('./website/douyin');
+const bilibili = require('./website/bilibili')
 
 export function getStreamUrl(url:string){
-    const result:any = kugou.main(url);
+    const result:any = bilibili.main(url);
 
     result.then(function(value:any) {
         console.log('the stream url is:');
         console.log(value);
       }, function(error:any) {
+          console.log('ERROR:');
         console.log(error);
       });
 
