f03a31b1897e2f26c603e3df1e75700f2610fb78
mail@nixiaobai.com <mail@nixiaobai.com>
2020-07-11 13:25:42 +0800

    添加酷狗繁星直播链接获取

--- src/getUrl.ts
+++ src/getUrl.ts
@@ -1,8 +1,9 @@
 //const huya = require('./website/huya');
-const douyu = require('./website/douyu');
+//const douyu = require('./website/douyu');
+const kugou = require('./website/kugou');
 
 export function getStreamUrl(url:string){
-    const result:any = douyu.main(url);
+    const result:any = kugou.main(url);
 
     result.then(function(value:any) {
         console.log('the stream url is:');
