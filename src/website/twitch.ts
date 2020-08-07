94660c67f4e94726fc9471983c9055ed0bc0db43
mail@nixiaobai.com <mail@nixiaobai.com>
2020-08-07 20:35:05 +0800

    add:twitch

--- src/website/twitch.ts
+++ src/website/twitch.ts
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const urlencode = require('urlencode');
 
 export function main(url: string) {
     return new Promise(function (resolve, reject) {
@@ -15,7 +16,6 @@ export function main(url: string) {
             method: "get",
             url: link,
             headers: {
-                "Client-ID": "15ss9u1dmrhxouwhcbb9g68j2ex2sp",
                 Accept: "application/vnd.twitchtv.v5+json",
                 "User-Agent":
                     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
@@ -27,8 +27,12 @@ export function main(url: string) {
 
         axios(config)
             .then(function (response: any) {
-                //console.log(JSON.stringify(response.data));
-                resolve(JSON.stringify(response.data))
+                const jsons:any = response.data;
+                let token:string = jsons.token;
+                const sig:string = jsons.sig;
+                const m3u8_url = `https://usher.ttvnw.net/api/channel/hls/${rid[0]}.m3u8?allow_source=true&fast_bread=true&player_backend=mediaplayer&playlist_include_framerate=true&reassignments_supported=true&sig=${sig}&supported_codecs=vp09%2Cavc1&token=${urlencode(token)}&cdm=wv&player_version=1.0.1`;
+
+                resolve(m3u8_url)
             })
             .catch(function (error: any) {
                 reject(error);
