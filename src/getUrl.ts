737403b14a80706df0a1b8060ba4bf3d2bca979c
mail@nixiaobai.com <mail@nixiaobai.com>
2020-08-07 20:39:31 +0800

    fix(getUrl.ts):catch error

--- src/getUrl.ts
+++ src/getUrl.ts
@@ -27,15 +27,12 @@ export function getStreamUrl(url: string) {
         const result: any = website.main(url);
 
         result.then(
-            function (value: any) {
-                console.log("the stream link is:");
+            (value: any) => {
                 console.log(value);
-            },
-            function (error: any) {
-                console.log("ERROR:");
-                console.log(error);
             }
-        );
+        ).catch((error: any) => {
+            console.log(error);
+        });
     } else {
         console.log(`This link is not supported`);
     }
